abdbf1ccf8fd51956dab7c8f8fb7ab5f4babb2b5
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2024-08-21 21:28:04 +0700

    add max glutton atcoder

--- Atcoder/Beginner/364/E_Maximum_Glutton.cpp
+++ Atcoder/Beginner/364/E_Maximum_Glutton.cpp
@@ -1,39 +1,70 @@
 #include <algorithm>
-#include <bitset>
 #include <climits>
-#include <functional>
 #include <iostream>
 #include <limits>
-#include <map>
-#include <numeric>
 #include <queue>
-#include <ranges>
-#include <set>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 using i64 = int64_t;
+int N;
+vector<pair<int, int>> A;
+int X, Y;
 
-void solution() {
-  int N;
+int dp()
+{
+  vector<vector<vector<int>>> dp = vector(N + 1, vector(N + 1, vector<int>(X + 1, INT_MAX)));
+
+  dp[0][0][0] = 0;
+
+  for (int i = 0; i < N; i++)
+  {
+    auto [dx, dy] = A[i];
+    for (int j = 0; j <= i; j++)
+    {
+      for (int k = 0; k <= X + 1; k++)
+      {
+        dp[i + 1][j][k] = min(dp[i + 1][j][k], dp[i][j][k]);
+        if (k + dx <= X && dp[i][j][k] != INT_MAX)
+        {
+          dp[i + 1][j + 1][k + dx] = min(dp[i + 1][j + 1][k + dx], dp[i][j][k] + dy);
+        }
+      }
+    }
+  }
+
+  for (int j = N; j >= 0; j--)
+  {
+    for (int k = X; k >= 0; k--)
+    {
+      if (dp[N][j][k] <= Y)
+      {
+        return min(j + 1, N);
+      }
+    }
+  }
+
+  return 0;
+}
+
+void solution()
+{
+
   cin >> N;
-  int X, Y;
   cin >> X >> Y;
 
-  vector<pair<int, int>> A(N);
+  A.resize(N);
 
   for (auto &a : A)
     cin >> a.first >> a.second;
 
-  int dp[N + 1][10'000 + 1]; // minimal second for X and count
-
-  for (auto [fa, sa] : A) {
-                
-  }
+  cout << dp() << endl;
 }
 
-int main() {
+int main()
+{
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
