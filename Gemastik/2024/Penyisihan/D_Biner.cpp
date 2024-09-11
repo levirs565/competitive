a5510da449ab6d6863e0c3b53c9bdb2c6626bbd3
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2024-09-11 13:22:27 +0700

    add biner

--- Gemastik/2024/Penyisihan/D_Biner.cpp
+++ Gemastik/2024/Penyisihan/D_Biner.cpp
@@ -13,49 +13,32 @@
 using namespace std;
 using i64 = int64_t;
 
-constexpr i64 maxN = 61;
+constexpr i64 maxN = 63;
 i64 C[maxN][maxN];
 
-void precompute() {
-  C[0][0] = 1;
-  for (int i = 1; i <= 60; i++) {
-    C[i][0] = 1;
-    for (int j = 1; j <= i; j++) {
-      C[i][j] = C[i - 1][j] + C[i - 1][j - 1];
-    }
-  }
-}
-
-i64 get(i64 m, i64 k) { return C[m][k]; }
-
 int main() {
-  precompute();
-  i64 N = 30, K;
-  cin >> K;
-
-  string s = "";
-  for (int a = 0; a <= 60; a++)
-    s += "0";
-
-  int last = -1;
-  for (i64 i = 60; i >= 0 && N; --i) {
-    if (get(i, N) > K)
-      continue;
-    else if (get(i, N) < K) {
-      s[i] = '1';
-      K -= get(i, N);
-      N--;
-    } else {
-      last = i;
-      break;
+    for (int n = 0; n < maxN; n++) {
+        C[n][0] = 1;
+        for (int k = 1; k <= n; k++) {
+            C[n][k] = C[n - 1][k - 1] + C[n - 1][k];
+        }
     }
-  }
 
-  for (int j = last - 1; j >= 0 && N; --j) {
-    s[j] = '1';
+    i64 N;
+    cin >> N;
     N--;
-  }
 
-  reverse(s.begin(), s.end());
-  cout << strtoull(s.c_str(), 0, 2) << endl;
+    int one = 30;
+
+    string result(63, '0');
+
+    for (int i = 0; i < 63; i++) {
+        if (N >= C[63 - i - 1][one]) {
+            N -= C[63 - i - 1][one];
+            one--;
+            result[i] = '1';
+        }
+    }
+
+    cout << strtoull(result.c_str(), nullptr, 2) << endl;
 }
