1b155cf037b1fb7f79db7c1b9e00bd592c0a57a5
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2024-12-29 15:25:51 +0700

    library checker

--- Library Checker/Data Structure/Range_Affine_Point_Get_LazySeg.cpp
+++ Library Checker/Data Structure/Range_Affine_Point_Get_LazySeg.cpp
@@ -12,8 +12,20 @@ i64 mod = 998244353;
 struct Node
 {
     i64 a, b, c;
-    bool isLazy = false;
+    // bx + c
 };
+
+Node compose(Node f1, Node f2)
+{
+    // f2 then f1
+    // x1 = b2 x2 + c2
+    // x0 = b1 x1 + c1 = b1 (b2 x2 + c2) + c1 = b1 b2 x2 + b1c2 + c1
+    Node res;
+    res.b = (f1.b * f2.b) % mod;
+    res.c = ((f1.b * f2.c) % mod + f1.c) % mod;
+    res.a = f2.a;
+    return res;
+}
 
 vector<Node> seg;
 vector<i64> a;
@@ -22,48 +34,33 @@ void build(int t, int l, int r)
 {
     if (l == r)
     {
-        seg[t] = {a[l], 0, 0, false};
+        seg[t] = {a[l], 1, 0};
     }
     else
     {
         int mid = (l + r) / 2;
         build(2 * t + 1, l, mid);
         build(2 * t + 2, mid + 1, r);
-        seg[t] = {0, 0, 0, false};
+        seg[t] = {0, 1, 0};
     }
 }
 
-void pull_down(int t, int l, int r)
+void push(int t, int l, int r)
 {
-    if (!seg[t].isLazy)
-        return;
-
-    if (l == r)
-    {
-        Node &n = seg[t];
-        n.a = ((n.a * n.b) % mod + n.c) % mod;
-        n.isLazy = false;
-        return;
-    }
-
-    int mid = (l + r) / 2;
-    pull_down(2 * t + 1, l, mid);
-    pull_down(2 * t + 2, mid + 1, r);
-
-    seg[2 * t + 1].b = seg[2 * t + 2].b = seg[t].b;
-    seg[2 * t + 1].c = seg[2 * t + 2].c = seg[t].c;
-    seg[2 * t + 1].isLazy = seg[2 * t + 2].isLazy = true;
-    seg[t].isLazy = false;
+    seg[2 * t + 1] = compose(seg[t], seg[2 * t + 1]);
+    seg[2 * t + 2] = compose(seg[t], seg[2 * t + 2]);
+    seg[t].b = 1;
+    seg[t].c = 0;
 }
 
 Node pget(int t, int l, int r, int i)
 {
-    pull_down(t, l, r);
-
     if (l == r)
     {
         return seg[t];
     }
+
+    push(t, l, r);
     int mid = (l + r) / 2;
     if (i <= mid)
     {
@@ -77,17 +74,15 @@ void rupdate(int t, int l, int r, int tl, int tr, i64 b, i64 c)
     if (l > tr || r < tl)
         return;
 
-    pull_down(t, l, r);
-
     if (l >= tl && r <= tr)
     {
-        seg[t].b = b;
-        seg[t].c = c;
-        seg[t].isLazy = true;
+        seg[t] = compose(Node{0, b, c}, seg[t]);
     }
     else
     {
         int mid = (l + r) / 2;
+
+        push(t, l, r);
         rupdate(2 * t + 1, l, mid, tl, min(tr, mid), b, c);
         rupdate(2 * t + 2, mid + 1, r, max(mid + 1, tl), tr, b, c);
     }
@@ -124,10 +119,13 @@ void solution()
 
             cin >> l >> r >> b >> c;
             rupdate(0, 0, N - 1, l, r - 1, b, c);
-        } else {
+        }
+        else
+        {
             int i;
             cin >> i;
-            cout << pget(0, 0, N - 1, i).a << endl;            
+            Node n = pget(0, 0, N - 1, i);
+            cout << ((n.a * n.b) % mod + n.c) % mod << endl;
         }
     }
 }
